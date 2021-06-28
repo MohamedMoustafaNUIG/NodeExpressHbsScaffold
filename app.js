4311fe6247a24626910172d57b54af280d3f102a
MohamedMoustafaNUIG <mohamedmoustafaofficial@gmail.com>
2021-06-28 22:08:48 +0100

    updated some packages

--- app.js
+++ app.js
@@ -4,11 +4,6 @@ const path = require('path');
 
 const app = express();
 
-//view engine setup
-/*app.use(express.static(path.join(__dirname, './views')));
-app.engine('express-handlebars', exphbs({ defaultLayout: 'layout'}));
-app.set('view engine', 'express-handlebars');
-*/
 app.engine('.hbs', exphbs({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 app.use(express.static(path.join(__dirname, './views')));
@@ -19,7 +14,7 @@ app.use(express.urlencoded({extended:false}))
 
 /* GET home page. */
 app.get('/', (req, res)=>{
-    res.render('index', {name: "Mohamed"})
+    res.render('index', {name: "Visitor"})
 })
 
 app.listen(process.env.PORT || 3000);
